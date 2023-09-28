a57e45ae1f086effcf5faa8974f6ff0ea4fba690
minu21shahabas <minu27theparambil@gmail.com>
2023-09-27 18:58:23 -0700

    first commit

---
+++ zohoapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import AddItem,Purchase,Sales,Unit
+
+# Register your models here.
+admin.site.register(AddItem)
+admin.site.register(Purchase)
+admin.site.register(Sales)
+admin.site.register(Unit)
+# admin.site.register(Purchase_Order)
